0d13599c0356b929087f2b23e8d4e4bf6bcaa47a
seunoduez@gmail.com <seunoduez@gmail.com>
2022-06-09 12:57:13 +0100

    signup and login reviewed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,56 @@
 const asyncHandler = require('express-async-handler')
-const {createUser, checkLoginDetails, getUserById, getUserByIdAndUpdate} = require('../functions/userFunctions')
+const {
+    createUser, 
+    checkLoginDetails, 
+    getUserByEmail, 
+    getUserById, 
+    getUserByIdAndUpdate, 
+    extractPassword,
+    checkPasswords, 
+    checkIfUsernameExists, 
+    checkIfEmailExists, 
+    hashEnteredPassword,
+    checkIfEnteredPasswordEqualsHashed
+} = require('../functions/userFunctions')
 const {generateToken} = require('../config/auth')
 
 
 // @desc     Create user account
 // @route    POST/e-commerce/users/signUp
 const signUpUser = asyncHandler(async (req, res) => {
-    if (req.body.username && req.body.first_name && req.body.last_name && req.body.email && req.body.address && req.body.phone_number && req.body.is_admin && req.body.password && req.body.confirm_password) {
+    const {username, first_name, last_name, email, address, phone_number, is_admin, password, confirm_password} = req.body
+    if (username && first_name && last_name && email && address && phone_number && is_admin && password && confirm_password) {
         try {
-            const newUser = await createUser(req.body.username, req.body.first_name, req.body.last_name, req.body.email, req.body.address, req.body.phone_number, req.body.is_admin, req.body.password)
-            res.status(201).json(newUser)
+            const checkUsername = await checkIfUsernameExists(username)
+            if (checkUsername === false) {
+                const checkEmail = await checkIfEmailExists(email)
+                if (checkEmail === false ) {
+                    const checkBothPasswords = await checkPasswords(password, confirm_password)
+                    if (checkBothPasswords == true) {
+                        const hashedPassword = await hashEnteredPassword(password)
+                        await createUser(username, first_name, last_name, email, address, phone_number, is_admin, hashedPassword)
+                        res.status(201).json([{username, first_name, last_name, email, address, phone_number, is_admin, password}])
+                    }
+                    else {
+                        res.status(400).json({
+                            errno:"115" ,
+                            message : "Password and confirm password do not match"
+                        })
+                    }
+                }
+                else {
+                    res.status(400).send({
+                        errno:"113" ,
+                        message : "Cannot add an existing email."
+                    })
+                } 
+            }
+            else {
+                res.status(400).send({
+                    errno:"113" ,
+                    message : "Cannot add an existing username."
+                })
+            } 
         } 
         catch (error) {
             res.send({message : error.message})
@@ -27,20 +68,40 @@ const signUpUser = asyncHandler(async (req, res) => {
 // @route    POST/e-commerce/users/login
 // @access   Private
 const loginUser = async (req, res) => {
-    if (req.body.email && req.body.password) {
+    const {email, password} = req.body
+    if (email && password) {
         try {
-            const result = await checkLoginDetails(req.body.email, req.body.password)
-            const user = JSON.parse(JSON.stringify(result[0]))
-            const token = await generateToken(user)
+            const checkEmail = await checkIfEmailExists(email)
+            
+            if (checkEmail === true) {
+                const hashedPassword = await extractPassword(email)
+                const checkPassword = await checkIfEnteredPasswordEqualsHashed(password, hashedPassword)
 
-            if (result.length == 1) {
-                res.status(200).send({
-                    message : "You have successfully logged in.", 
-                    user, 
-                    token
+                if (checkPassword === true) {
+                    const userDetails = await getUserByEmail(email)
+                    const user = JSON.parse(JSON.stringify(userDetails[0]))
+
+                    const token = await generateToken(user)
+
+                    res.status(200).send({
+                        message : "You have successfully logged in.", 
+                        user, 
+                        token
+                    })
+                }
+                else {
+                    res.status(401).send({
+                        errno:"116" ,
+                        message : "Incorrect Password."
+                    })
+                }
+            }
+            else {
+                res.status(401).send({
+                    errno:"116" ,
+                    message : "Incorrect Email."
                 })
             }
-
         } catch (error) {
             res.send({message : error.message})
         }
@@ -57,7 +118,8 @@ const loginUser = async (req, res) => {
 // @route    PATCH/e-commerce/users/login/update_account_details
 // @access   Private
 const updateAccountDetails = async (req, res) => {
-    if (req.body.username && req.body.first_name && req.body.last_name && req.body.email && req.body.address && req.body.phone_number && req.body.is_admin) {
+    const {username, first_name, last_name, email, address, phone_number, is_admin} = req.body
+    if (username && first_name && last_name && email && address && phone_number && is_admin) {
         try {
             const user = await getUserById(req.user._id)
             if(!user) {
--- backend/functions/userFunctions.js
+++ backend/functions/userFunctions.js
@@ -1,49 +1,146 @@
-const res = require('express/lib/response')
 const User = require('../models/userModel')
+const bcrypt = require('bcrypt');
+
 
 function createUser (username, first_name, last_name, email, address, phone_number, is_admin, password) {
     return new Promise((resolve, reject) => {
-        const user = User.create({
-            username: username, 
-            first_name: first_name, 
-            last_name: last_name,
-            email: email,
-            address: address, 
-            phone_number: phone_number, 
-            is_admin: is_admin,
-            password: password,
+        const user = User.create({username, first_name, last_name, email, address, phone_number, is_admin, password}, (err, docs) => {
+            if (err) reject(err)
+            resolve(docs)
         })
-            .then(result => resolve(result))
-            .catch(error => reject(error))
     })
 }
 
 function checkLoginDetails (email, password) {
     return new Promise((resolve, reject) => {
-        const user = User.find({email: email, password: password})
-            .then(result => resolve(result))
-            .catch(error => reject(error))
+        User.find({email: email, password: password}, (err, docs) => {
+            if (err) reject(err)
+            resolve(docs)
+        })
     })
 }
-
-
 
 function getUserById (id) {
     return new Promise((resolve, reject) => {
-        const user = User.findById(id)
-            .then(result => resolve(result))
-            .catch(error => reject(error))
+        User.findById(id, (err, docs) => {
+            if (err) reject(err)
+            resolve(result)
+        })
     })
 }
 
-function getUserByIdAndUpdate (id, newDetails, obj) {
+function getUserByEmail (email) {
     return new Promise((resolve, reject) => {
-        const newUpdate = User.findByIdAndUpdate(id, newDetails, obj)
-            .then(result => resolve(result))
-            .catch(error => reject(error))
+        User.find({email}, (err, docs) => {
+            if (err) reject(err)
+            resolve(docs)
+        })
     })
 }
 
-const exported = {createUser, checkLoginDetails, getUserById, getUserByIdAndUpdate}
+function getUserByIdAndUpdate(id, newDetails, obj) {
+    return new Promise((resolve, reject) => {
+        User.findByIdAndUpdate(id, newDetails, obj, (err, docs) => {
+            if (err) reject (err)
+            resolve(docs)
+        })
+    })
+}
+
+function checkPasswords(password, confirm_password) {
+    return new Promise((resolve, reject) => {
+        if (password === confirm_password){
+            resolve(true)
+        } 
+        else {
+            resolve(false)
+        }
+    })
+}
+
+function checkIfUsernameExists (username) {
+    return new Promise((resolve, reject) => {
+        User.find({username}, (err, docs) => {
+            if (docs.length == 1) resolve(true)
+            else resolve(false)            
+        })
+    })
+}
+
+function checkIfEmailExists (email) {
+    return new Promise((resolve, reject) => {
+        User.find({email}, (err, docs) => {
+            if (docs.length == 1)  resolve(true)
+            else resolve(false)
+        })
+    })
+}
+
+function extractPassword (email) {
+    return new Promise((resolve, reject) => {
+        User.find({email}, (err, docs) => {
+            if (err) reject(err)
+            resolve(docs[0].password)
+        })
+    })
+}
+
+// extractPassword('seunoduez@gmail.com')
+//     .then(result => console.log(result))
+
+
+function checkIfEnteredPasswordEqualsHashed(password, hashedPassword) {
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(password, hashedPassword, function(err, result) {
+            if (err) reject(err)
+            resolve(result)
+        });
+    })
+}
+
+// async function checkIfEmailExists3 (email) {
+//     try {
+//         const user = await User.find({email})
+//         return user
+//     } catch (error) {
+//         throw (error)
+//     }
+// }
+
+
+
+
+// function checkIfEmailExists (email) {
+//     return new Promise((resolve, reject) => {
+//         const user = User.find({email: email})
+//             .then(resolve(true))
+//             .catch(reject(false))
+//     })
+// }
+
+function hashEnteredPassword(password) {
+    return new Promise((resolve, reject) => {
+        const saltRounds = 10;
+        bcrypt.hash(password, saltRounds, function(err, hash) {
+            resolve(hash)
+            reject(err)
+        });
+    })
+}
+
+
+const exported = {
+    createUser, 
+    checkLoginDetails, 
+    getUserById,
+    getUserByEmail, 
+    getUserByIdAndUpdate, 
+    checkPasswords, 
+    checkIfUsernameExists,
+    checkIfEmailExists, 
+    extractPassword, 
+    hashEnteredPassword, 
+    checkIfEnteredPasswordEqualsHashed
+}
 
 module.exports = exported
